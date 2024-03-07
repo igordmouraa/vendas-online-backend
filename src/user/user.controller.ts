49a7e2a48a62dbfff8e1eb732436891e0c1bc628
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-07 05:01:22 -0300

    feat: User Controller with Post method

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return this.userService.createUser(createUser);
+  }
+}

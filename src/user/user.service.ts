c8f53e326c58717084a8f729eccabb2772fad8ed
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-07 05:03:43 -0300

    feat: Create the function createUser service

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+    private users: User[] = [];
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const saltOrRounds = 10;
+        const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+        
+        const user = {
+            ...createUserDto,
+            id: this.users.length + 1,
+            password: passwordHashed,
+        };
+
+        this.users.push(user);
+
+        console.log('passwordHashed', passwordHashed);
+        
+
+        return user;
+    }
+}

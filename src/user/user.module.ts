31812ed78caf9a9d35b0c25487f4cd7b9ef5bdfc
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-11 22:03:28 -0300

    :sparkles: feat: Create a userRepository using typeOrm forFeature

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './interfaces/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService]
 })

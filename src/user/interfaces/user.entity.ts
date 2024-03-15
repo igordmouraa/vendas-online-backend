dbb0dbd3a7bfba69e546d039c4f316f26bcc9f68
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-15 18:20:52 -0300

    :sparkles: feat: Create a 'created_at', 'updated_at' and 'type_user' columns

--- src/user/interfaces/user.entity.ts
+++ src/user/interfaces/user.entity.ts
@@ -1,18 +1,32 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: 'user'})
 export class UserEntity{
 
     @PrimaryGeneratedColumn('rowid')
     id: number;
+
     @Column({name: 'name', nullable: false})
     name: string;
+    
     @Column({name: 'email', nullable: false})
     email: string;
+
     @Column({name: 'phone'})
     phone: string;
+
     @Column({name: 'cpf', nullable: false})
     cpf: string;
+
     @Column({name: 'password', nullable: false})
     password: string;
+
+    @Column({name: 'type_user', nullable: false})
+    typeUser: number;
+
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: Date;
+
+    @UpdateDateColumn({name: 'updated_at'})
+    updatedAt: Date;
 }

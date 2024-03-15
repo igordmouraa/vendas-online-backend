a31ae63014a81d58254fc3ab65f1c6b603200976
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-15 20:43:47 -0300

    :sparkles: feat: Create getAllCitiesByStateId function

---
+++ src/city/city.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { CityService } from './city.service';
+
+@Controller('city')
+export class CityController {
+
+    constructor(
+        private readonly cityService: CityService,
+    ){}
+
+
+    @Get('/:stateId')
+    async getAllCitiesByStateId(@Param('stateId') stateId: number,): Promise<CityEntity[]>{
+        return this.cityService.getAllCitiesByStateId(stateId);
+    }
+}

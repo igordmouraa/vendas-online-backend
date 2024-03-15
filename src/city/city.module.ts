38980de6d779f4a31e228ce43c90336a81c070c8
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-15 20:45:17 -0300

    :sparkles: feat: Create a typeOrmModule - cityEntity

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { CityEntity } from './entities/city.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CityEntity])],
+  controllers: [CityController],
+  providers: [CityService]
+})
 export class CityModule {}

3338553d755289ef64b6512c5acb6e0b28597f33
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-15 18:22:01 -0300

    :sparkles: feat: Added typeUser on 'userRepository.save'.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,6 +20,7 @@ export class UserService {
         
         return this.userRepository.save({
             ...createUserDto,
+            typeUser: 1,
             password: passwordHashed,
         })
     }

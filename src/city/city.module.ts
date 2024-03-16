9bd35bde89564f4f2a6e9dd82db624e8b9a5f9dc
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-16 04:16:58 -0300

    :zap: Added cacheManager to best time response

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '../cache/cache.module';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { CityEntity } from './entities/city.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CityEntity])],
+  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
-  providers: [CityService]
+  providers: [CityService],
+  exports: [CityService],
 })
 export class CityModule {}

d80ef39135dc599ed0b3811bce43d32cc60ed393
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-17 22:13:36 -0300

    :sparkles: feat: Added TypeOrm feature AddressEntity

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AddressService } from './address.service';
+import { AddressController } from './address.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressEntity } from './entities/address.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  providers: [AddressService],
+  controllers: [AddressController]
+})
 export class AddressModule {}

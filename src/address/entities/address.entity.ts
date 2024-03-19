576350e73e2195e38455f0ff166f7727b0c388d9
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-19 00:15:10 -0300

    :sparkles: feat: Added TypeOrm relations One-to-Many | Many-to-One

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,4 +1,6 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CityEntity } from "src/city/entities/city.entity";
+import { UserEntity } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: 'address'})
 export class AddressEntity{
@@ -26,4 +28,12 @@ export class AddressEntity{
 
     @UpdateDateColumn({name: 'updated_at'})
     updatedAt: Date;
+
+    @ManyToOne(() => UserEntity, (user) => user.addresses)
+    @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+    user?: UserEntity;
+
+    @ManyToOne(() => CityEntity, (city) => city.addresses)
+    @JoinColumn({ name: 'city_id', referencedColumnName: 'id' })
+    city?: CityEntity;
 }

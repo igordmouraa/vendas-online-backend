e80219ada517a4ed7b0e6b7c8741461bf2ffeeb0
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-19 11:08:32 -0300

    :recycle: refactor: Refactored the getUserByIdUsingRelations function for better data handling

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -30,7 +30,13 @@ export class UserService {
             where: {
                 id: userId,
             },
-            relations: ['addresses'],
+            relations: {
+                addresses: {
+                    city: {
+                        state: true,
+                    }
+                },
+            },
         });
     }
 

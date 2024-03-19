59bb0926ed6905df381bd49a4d9ff665cfe1fba4
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-18 23:19:44 -0300

    :sparkles: feat: Added(import) user and city Module

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -3,9 +3,11 @@ import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressEntity } from './entities/address.entity';
+import { UserModule } from 'src/user/user.module';
+import { CityModule } from 'src/city/city.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  imports: [TypeOrmModule.forFeature([AddressEntity]), UserModule, CityModule],
   providers: [AddressService],
   controllers: [AddressController]
 })

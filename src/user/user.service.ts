f5d90c3fe99ce1d29b7728c8fddc0462d08ce3a4
Igor de Moura <127807075+igordmouraa@users.noreply.github.com>
2024-03-18 23:27:19 -0300

    :sparkles: feat: Create findUserById function

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
@@ -11,13 +11,13 @@ export class UserService {
     constructor(
         @InjectRepository(UserEntity)
         private readonly userRepository: Repository<UserEntity>,
-    ){}
+    ) { }
 
 
     async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
         const saltOrRounds = 10;
         const passwordHashed = await hash(createUserDto.password, saltOrRounds);
-        
+
         return this.userRepository.save({
             ...createUserDto,
             typeUser: 1,
@@ -25,7 +25,21 @@ export class UserService {
         })
     }
 
-    async getAllUser(): Promise<UserEntity[]>{
+    async getAllUser(): Promise<UserEntity[]> {
         return this.userRepository.find();
+    }
+
+    async findUserById(userId: number): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({
+            where: {
+                id: userId,
+            }
+        });
+
+        if (!user){
+            throw new NotFoundException(`UserId: ${userId} is not found`);
+        }
+
+        return user;
     }
 }
